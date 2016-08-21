efcb2697d442d9d548170094e95dad946f098fa6
Alex Red <l3dz@ya.ru>
2016-08-22 01:08:22 +0300

    switch to use Hiera in Vagrant

--- .vagrant_puppet/environments/vagrant/manifests/init.pp
+++ .vagrant_puppet/environments/vagrant/manifests/init.pp
@@ -1,34 +1,34 @@
-#hiera_include('classes')
+hiera_include('classes')
 
 
-node 'client1.example.net' {
-  class { 'nfs::server::exports':
-    share => {
-      'vol1' => {
-        path    => '/vol1',
-        acl     => '192.168.50.0/24',
-        options => 'rw,sync,no_root_squash'},
-      'vol2' => {
-        path    => '/vol2',
-        acl     => '192.168.50.0/24',
-        options => 'rw,sync,no_root_squash'},
-      },
-    }
-}
+#node 'client1.example.net' {
+#  class { 'nfs::server::exports':
+#    share => {
+#      'vol1' => {
+#        path    => '/vol1',
+#        acl     => '192.168.50.0/24',
+#        options => 'rw,sync,no_root_squash'},
+#      'vol2' => {
+#        path    => '/vol2',
+#        acl     => '192.168.50.0/24',
+#        options => 'rw,sync,no_root_squash'},
+#      },
+#    }
+#}
 
-node 'client2.example.net' {
-  class { 'nfs::client::mounts':
-    share => {
-      'vol1' => {
-        ensure     => present,
-        share      => '/vol1',
-        server     => '192.168.50.4',
-        mountpoint => '/vol1'},
-      'vol2' => {
-        ensure     => present,
-        share      => '/vol2',
-        server     => '192.168.50.4',
-        mountpoint => '/vol2'},
-    },
-  }
-}
+#node 'client2.example.net' {
+#  class { 'nfs::client::mounts':
+#    share => {
+#      'vol1' => {
+#        ensure     => present,
+#        share      => '/vol1',
+#        server     => '192.168.50.4',
+#        mountpoint => '/vol1'},
+#      'vol2' => {
+#        ensure     => present,
+#        share      => '/vol2',
+#        server     => '192.168.50.4',
+#        mountpoint => '/vol2'},
+#    },
+#  }
+#}
