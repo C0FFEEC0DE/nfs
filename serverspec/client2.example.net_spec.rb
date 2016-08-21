0710d9fc8edef46a29d553c305f7e3c4df828b5b
Alex Red <l3dz@ya.ru>
2016-08-22 01:06:57 +0300

    add serverspec test

--- serverspec/client2.example.net_spec.rb
+++ serverspec/client2.example.net_spec.rb
@@ -1,10 +1,17 @@
 # For serverspec documentation - see http://serverspec.org/tutorial.html
 
 describe 'nfs-common' do
+
   describe package('nfs-common') do
     it { should be_installed }
   end
+
   describe file('/vol1') do
   it { should be_mounted.with( :type => 'nfs' ) }
   end
+
+  describe file('/vol2') do
+  it { should be_mounted.with( :type => 'nfs' ) }
+  end
+
 end
